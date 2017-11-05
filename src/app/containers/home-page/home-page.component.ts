5f77979fa7968d37fc0e51cffe4741643fb9683d
Antoine Muller <nemak121@gmail.com>
2017-11-05 13:08:18 +0100

    Add loader

--- src/app/containers/home-page/home-page.component.ts
+++ src/app/containers/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs'
 import { Component } from '@angular/core';
 import { State } from '../../reducers'
 import { Store } from '@ngrx/store';
+import { LoadingController, Loading } from 'ionic-angular';
 import * as fromDogs from '../../actions/dogs';
 
 @Component({
@@ -11,6 +12,10 @@ import * as fromDogs from '../../actions/dogs';
 export class HomePage {
 
   dogImgUrl$: Observable<string>
+  isLoading$: Observable<boolean>
+
+  // Used to display the loader
+  _loader: Loading
 
   /*
    * The store is injected in the constructor by angular
@@ -18,11 +23,18 @@ export class HomePage {
    * The store will also be used to dispatch action to the store on certain event
    * The State is the global state of the application. It is used by typescript help the developper
    */
-  constructor(private _store: Store<State>) {
+  constructor(private _store: Store<State>, private _loadingCtrl: LoadingController) {
     /*
      * We are getting the imgUrl from the store
      */
     this.dogImgUrl$ = _store.select(state => state.dogs.currentDog.imgUrl)
+
+    // Get the "isLoading" boolean from the store
+    this.isLoading$ = _store.select(state => state.dogs.isLoading)
+
+    // Each time isLoading change, subscribe callback is called
+    this.isLoading$.subscribe(isLoading => this._updateLoader(isLoading))
+
   }
 
   /*
@@ -31,5 +43,18 @@ export class HomePage {
   onFindAnotherDogClicked() {
     // Dispatch the "findAnotherDog" action using the _store injected in the constructor
     this._store.dispatch(new fromDogs.fetchRandomDog())
+  }
+
+  _updateLoader(isLoading: boolean) {
+    if (isLoading) {
+      // create a new loader and display it
+      this._loader = this._loadingCtrl.create();
+      this._loader.present()
+    } else {
+      // hide the loader
+      if (this._loader) {
+        this._loader.dismiss()
+      }
+    }
   }
 }
